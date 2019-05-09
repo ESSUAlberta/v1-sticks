632343c0260f82ff5e069d50f81ff29c10f6c47e
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2019-05-09 00:48:08 -0600

    temporary commit to eject

---
+++ gallery/src/ButtonPage/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from '@essua/v1-sticks'
+
+class ButtonPage extends Component {
+  render() {
+    return (
+      <div>
+        <Button></Button>
+      </div>
+    )
+  }
+}
+
+export default ButtonPage
--- gallery/src/Router.js
+++ gallery/src/Router.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ButtonPage from './ButtonPage'
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
-            <Route component={() => null} path={"/"} />
+            <Route exact component={() => null} path={"/"} />
+            <Route exact component={ButtonPage} path={"/Button"} />
+            <Route component={() => null} />
         </Switch>
     </BrowserRouter>
 );
